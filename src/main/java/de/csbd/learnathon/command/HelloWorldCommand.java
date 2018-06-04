2585bc6fd1cea2672e99c6f9c5737d52ec443d87
Benjamin Wilhelm <benjamin.wilhelm@uni-konstanz.de>
2018-06-04 13:42:45 +0200

    Add parameter output and format

--- src/main/java/de/csbd/learnathon/command/HelloWorldCommand.java
+++ src/main/java/de/csbd/learnathon/command/HelloWorldCommand.java
@@ -8,11 +8,12 @@
 
 package de.csbd.learnathon.command;
 
+import org.scijava.ItemIO;
 import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
 import net.imagej.ImageJ;
-import net.imglib2.type.numeric.RealType;
 
 /**
  * This example illustrates how to create an ImageJ {@link Command} plugin.
@@ -21,25 +22,27 @@ import net.imglib2.type.numeric.RealType;
  * </p>
  */
 @Plugin(type = Command.class, menuPath = "Plugins>Hello World")
-public class HelloWorldCommand<T extends RealType<T>> implements Command {
+public class HelloWorldCommand implements Command {
 
-    @Override
-    public void run() {
-    	System.out.println("Hello World");
-    }
+	@Parameter(type = ItemIO.OUTPUT)
+	private String out;
 
-    /**
-     * This main function serves for development purposes.
-     * It allows you to run the plugin immediately out of
-     * your integrated development environment (IDE).
-     *
-     * @param args whatever, it's ignored
-     * @throws Exception
-     */
-    public static void main(final String... args) throws Exception {
-        // create the ImageJ application context with all available services
-        final ImageJ ij = new ImageJ();
-        ij.ui().showUI();
-    }
+	@Override
+	public void run() {
+		out = "Hello World";
+	}
 
+	/**
+	 * This main function serves for development purposes. It allows you to run the
+	 * plugin immediately out of your integrated development environment (IDE).
+	 *
+	 * @param args
+	 *            whatever, it's ignored
+	 * @throws Exception
+	 */
+	public static void main(final String... args) throws Exception {
+		// create the ImageJ application context with all available services
+		final ImageJ ij = new ImageJ();
+		ij.ui().showUI();
+	}
 }
